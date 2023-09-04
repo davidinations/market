d025b569ffeb0b8cfba088ce25d35ff9ac1a28ad
davidinations <david19002@mail.unpad.ac.id>
2023-09-04 21:47:20 +0700

    Tambah + Update File Baru

--- src/main.py
+++ src/main.py
@@ -1,97 +1,49 @@
-# Program Lama
+import fruitmarket as fm
 
-# while True:
-#     nApel = int(input("Masukkan Jumlah Apel : "))
-#     print(f'Masukkan Jumlah Apel : {nApel}')
-#     if nApel >= stockApel:
-#         print(f'Jumlah yang dimasukkan terlalu banyak. stock Apel Tinggal {stockApel}')
-#         continue
-#     else:
-#         stockApel -= nApel
-#         break
-    
-# while True:
-#     nJeruk = int(input("Masukkan Jumlah Jeruk : "))
-#     print(f'Masukkan Jumlah Jeruk : {nJeruk}')
-#     if nJeruk >= stockJeruk:
-#         print(f'Jumlah yang dimasukkan terlalu banyak. stock Jeruk Tinggal {stockJeruk}')
-#         continue
-#     else:
-#         stockJeruk -= nJeruk
-#         break
+# List Buah
+listBuah = ['Apel', 'Jeruk', 'Anggur']
+# List Harga
+listHarga = [10000, 15000, 20000]
+# List Stock
+listStock = [10, 10, 10]
 
-# while True:
-#     nAnggur = int(input("Masukkan Jumlah Anggur : "))
-#     print(f'Masukkan Jumlah Anggur : {nAnggur}')
-#     if nAnggur >= stockAnggur:
-#         print(f'Jumlah yang dimasukkan terlalu banyak. stock Anggur Tinggal {stockAnggur}')
-#         continue
-#     else:
-#         stockAnggur -= nAnggur
-#         break
+# List Keranjang
+keranjangBuah = []
+keranjangHarga = []
+keranjangStock = []
+keranjangTotal = []
 
+# define PROMPT message
+PROMPT = '''Selamat Datang Di Pasar Buah \n
+List Menu:
+1. Menampilkan Daftar Buah
+2. Menambah Buah
+3. Menghapus Buah
+4. Membeli Buah
+5. Exit The Program
+Masukkan Angka Menu yang ingin dijalankan: '''
 
-# Program Baru
-def input_fruit(name, stock, price):  
-    """
-    _Summary_
-
-    Args:
-        name (str): definisikan nama buah yang sesuai dengan variabel yang ada
-        stock (int): definisikan jumlah stok buah yang ada pada variabel stock
-        price (int): definisikan harga buah yang ada pada variabel price
-
-    Returns:
-        totalHargaPerBuah (int): mengembalikan total harga masing masing buah
-    """    
+def main():
     while True:
-        n = int(input(f"Masukkan Jumlah {name.capitalize()} : "))
-        if n >= stock:
-            print(f'Jumlah yang dimasukkan terlalu banyak. stock {name.capitalize()} Tinggal {stock}')
+        inputan = int(input(PROMPT))
+        if inputan == 1:
+            fm.show(listBuah, listHarga, listStock)
             continue
-        else:
-            price = n * price
-            stock -= n
-            break
-    return price, n
 
-# Stock dan Harga
-stockApel = 10
-stockJeruk = 10
-stockAnggur = 10
+        elif inputan == 2:
+            fm.add(listBuah, listHarga, listStock)
+            continue
 
-priceApel = 10000
-priceJeruk = 15000
-priceAnggur = 20000
+        elif inputan == 3:
+            fm.remove(listBuah, listHarga, listStock)
+            continue
 
-# Total harga masing masing buah dan keseluruhan buah
-totalPriceApel, nApel = input_fruit('apel', stockApel, priceApel)
-totalPriceJeruk, nJeruk = input_fruit('jeruk', stockJeruk, priceJeruk)
-totalPriceAnggur, nAnggur = input_fruit('anggur', stockAnggur, priceAnggur)
-totalPriceOverall = totalPriceApel + totalPriceJeruk + totalPriceAnggur
+        elif inputan == 4:
+            fm.buy(listBuah, listHarga, listStock, keranjangBuah, keranjangHarga, keranjangStock, keranjangTotal)
+            continue
 
-# Output
-print(
-f'''
-Detail Belanja \n
-Apel: {nApel} x {priceApel} = {totalPriceApel}
-Jeruk: {nJeruk} x {priceJeruk} = {totalPriceJeruk}
-Anggur: {nAnggur} x {priceAnggur} = {totalPriceAnggur} \n
-Total: {totalPriceOverall}
-'''
-)
+        elif inputan == 5:
+            exit()
 
-while True:
-    payment = int(input("Masukkan Jumlah Uang : "))
-    print(f'Masukkan Jumlah Uang : {payment}')
-    if payment < totalPriceOverall:
-        print(f'Uang Anda Kurang Sebesar {totalPriceOverall-payment}\n')
-        continue
-    else:
-        print(f'Terima Kasih')
-        if payment != totalPriceOverall:
-            print(f'\nUang Kembalian Anda : {payment-totalPriceOverall}')
-        break
-
-
-
+if __name__ == '__main__':
+        main()
